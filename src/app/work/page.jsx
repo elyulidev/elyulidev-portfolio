1ca853632e5d54f3646f208cc99cf8b377ebbb88
elyulidev <elyuli.dev@gmail.com>
2024-03-01 15:18:37 +0200

    Work: Add real data

--- src/app/work/page.jsx
+++ src/app/work/page.jsx
@@ -1,5 +1,54 @@
+"use client";
+import Bulb from "@/components/Bulb";
+import Circles from "@/components/Circles";
+import WorkSlider from "@/components/WorkSlider";
+import { fadeIn } from "@/lib/variants";
+import { motion } from "framer-motion";
+
 const Work = () => {
-  return <div>Work</div>;
+	return (
+		<main className='h-full bg-primary/30 py-36 flex items-center'>
+			<Circles />
+			<div className='container mx-auto'>
+				<div className='flex flex-col md:flex-row gap-x-8'>
+					{/* text */}
+					<div className='min-w-[200px] text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0'>
+						<motion.h2
+							variants={fadeIn("up", 0.2)}
+							initial='hidden'
+							animate='show'
+							exit='hidden'
+							className='text-[35px] leading-tight xl:text-[54px] xl:leading-[1.3] mb-2 mt-8 sm:mt-32 md:mt-0 xl:mt-12 font-semibold'
+						>
+							My <span className='text-accent'>Works</span>
+						</motion.h2>
+						<motion.p
+							variants={fadeIn("up", 0.4)}
+							initial='hidden'
+							animate='show'
+							exit='hidden'
+							className=' max-w-[400px] mx-auto lg:mx-0'
+						>
+							Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat
+							dolor quod esse ab ratione laborum repellat quas id sed at, vero
+							nobis, totam cupiditate ad? Iure ipsum dolore at vero!
+						</motion.p>
+					</div>
+					<motion.section
+						variants={fadeIn("down", 0.6)}
+						initial='hidden'
+						animate='show'
+						exit='hidden'
+						className='w-full xl:max-w-[65%]'
+					>
+						{/* slider */}
+						<WorkSlider />
+					</motion.section>
+				</div>
+			</div>
+			<Bulb />
+		</main>
+	);
 };
 
 export default Work;
--- src/components/WorkSlider.jsx
+++ src/components/WorkSlider.jsx
@@ -1,51 +1,102 @@
+import { BsArrowRight } from "react-icons/bs";
+import { Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+import Image from "next/image";
 // data
 const workSlides = {
-  slides: [
-    {
-      images: [
-        {
-          title: 'title',
-          path: '/thumb1.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb2.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb3.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb4.jpg',
-        },
-      ],
-    },
-    {
-      images: [
-        {
-          title: 'title',
-          path: '/thumb4.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb1.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb2.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb3.jpg',
-        },
-      ],
-    },
-  ],
+	slides: [
+		{
+			images: [
+				{
+					title: "title",
+					path: "/thumb1.jpg",
+				},
+				{
+					title: "title",
+					path: "/thumb2.jpg",
+				},
+				{
+					title: "title",
+					path: "/thumb3.jpg",
+				},
+				{
+					title: "title",
+					path: "/thumb4.jpg",
+				},
+			],
+		},
+		{
+			images: [
+				{
+					title: "title",
+					path: "/thumb4.jpg",
+				},
+				{
+					title: "title",
+					path: "/thumb1.jpg",
+				},
+				{
+					title: "title",
+					path: "/thumb2.jpg",
+				},
+				{
+					title: "title",
+					path: "/thumb3.jpg",
+				},
+			],
+		},
+	],
 };
 
 const WorkSlider = () => {
-  return <div>Work Slider</div>;
+	return (
+		<Swiper
+			spaceBetween={10}
+			freeMode={true}
+			pagination={{ clickable: true }}
+			modules={[Pagination]}
+			className='h-[240px] sm:h-[480px]'
+		>
+			{workSlides.slides.map((slide, index) => (
+				<SwiperSlide key={index}>
+					<div className='grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer'>
+						{slide.images.map((image, index) => (
+							<div
+								key={index}
+								className='relative rounded-lg overflow-hidden flex items-center justify-center group'
+							>
+								<div className='flex items-center justify-center relative overflow-hidden group'>
+									{" "}
+									<Image src={image.path} alt='' width={500} height={200} />
+									{/* overlay gradient */}
+									<div className='absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700'></div>
+									{/* title */}
+									<div className='absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300'>
+										<div className='flex items-center gap-x-2 text-[13px] tracking-[0.2em]'>
+											{/* title part 1*/}
+											<div className='delay-100'>LIVE</div>
+											{/* title part 2*/}
+											<div className='translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150'>
+												PROJECT
+											</div>
+											{/* title part 3*/}
+											<div className=''>
+												<BsArrowRight />
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						))}
+					</div>
+				</SwiperSlide>
+			))}
+		</Swiper>
+	);
 };
 
 export default WorkSlider;
