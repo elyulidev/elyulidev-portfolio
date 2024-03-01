8bbba517d437e8508fcee61bfb8e919b8ea296c9
elyulidev <elyuli.dev@gmail.com>
2024-03-01 15:20:41 +0200

    Testimonial: Add real data and fix image

--- src/app/testimonials/page.jsx
+++ src/app/testimonials/page.jsx
@@ -1,5 +1,33 @@
+"use client";
+import TestimonialSlider from "@/components/TestimonialSlider";
+import { fadeIn } from "@/lib/variants";
+import { motion } from "framer-motion";
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+	return (
+		<main className='h-full bg-primary/30 py-32; text-center'>
+			<div className='container mx-auto h-full flex flex-col justify-center'>
+				{/* title */}
+				<motion.h2
+					variants={fadeIn("up", 0.2)}
+					initial='hidden'
+					animate='show'
+					exit='hidden'
+					className='text-[35px] leading-tight xl:text-[54px] xl:leading-[1.3] mb-2 xl:mb-0 mt-8 sm:mt-32 md:mt-0 font-semibold'
+				>
+					What clients <span className='text-accent'>say.</span>
+				</motion.h2>
+				{/* slider */}
+				<motion.div
+					variants={fadeIn("up", 0.4)}
+					initial='hidden'
+					animate='show'
+					exit='hidden'
+				>
+					<TestimonialSlider />
+				</motion.div>
+			</div>
+		</main>
+	);
 };
 
 export default Testimonials;
--- src/components/TestimonialSlider.jsx
+++ src/components/TestimonialSlider.jsx
@@ -1,30 +1,99 @@
+"use client";
+
+import { Navigation, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import Image from "next/image";
+import { FaQuoteLeft } from "react-icons/fa";
+
 // testimonial data
 const testimonialData = [
-  {
-    image: '/t-avt-1.png',
-    name: 'Anne Smith',
-    position: 'Customer',
-    message:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
-  },
-  {
-    image: '/t-avt-2.png',
-    name: 'Jane Doe',
-    position: 'Customer',
-    message:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
-  },
-  {
-    image: '/t-avt-3.png',
-    name: 'Jhon Doe',
-    position: 'Customer',
-    message:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
-  },
+	{
+		image: "bg-yoelkis",
+		name: "Yoelkis Hernandez Victor",
+		position: "Vice Chancellor at the University of Ciego de Ávila",
+		message:
+			"When you handed in my work my head was spinning, you were very nice and implemented all the features I requested. Thank you.",
+		aspectRatio: "256/341",
+	},
+	{
+		image: "bg-fito",
+		name: "Rodolfo Luis Reyes Baños",
+		position:
+			"Director of the Department of Networks at the University of Ciego de Avila",
+		message:
+			"I patronized Julio and when He delivered, I honestly fell in love with the project He is a very honest guy and he delivers ontime.",
+		aspectRatio: "128/85",
+	},
+	{
+		image: "bg-yordanis",
+		name: "Yordanis Arencibia López",
+		position: "Network Administrator at the University of Ciego de Ávila",
+		message:
+			"I have been working with him for seven years, I have seen his professional growth and his performance on the front page. I highly recommend it.",
+		aspectRatio: "256/193",
+	},
+	{
+		image: "bg-daniel",
+		name: "Daniel Orlando Jiménez Choi",
+		position: "Web Master at the University of Ciego de Ávila",
+		aspectRatio: "1/1",
+		message:
+			"Julio is a professional dedicated to self-improvement at all times. He is an incredible person, very responsible and deeply involved in our work team.",
+	},
 ];
 
 const TestimonialSlider = () => {
-  return <div>Testimonial Slider</div>;
+	return (
+		<Swiper
+			navigation={true}
+			freeMode={true}
+			//pagination={{ clickable: true }}
+			modules={[Navigation]}
+			className='h-[350px]'
+		>
+			{testimonialData.map((person, index) => (
+				<SwiperSlide key={index}>
+					<div className='flex flex-col items-center sm:flex-row gap-x-8 h-full px-4'>
+						{/* avatar, name, position */}
+						<div className='w-full max-w-[300px] flex flex-col xl:justify-center items-center relative mx-auto xl:mx-0'>
+							<div className='flex flex-col justify-center text-center'>
+								{/* avatar */}
+								<div
+									className={`${
+										person.image === "bg-yordanis" ? "bg-right" : "bg-center"
+									} ${
+										person.image
+									} mb-2 mx-auto bg-yoelkis bg-cover bg-no-repeat rounded-full w-[100px] h-[100px]`}
+								></div>
+								{/* name */}
+								<div className='text-lg'>{person.name}</div>
+								{/* position */}
+								<div className='uppercase text-[12px] font-extralight'>
+									{person.position}
+								</div>
+							</div>
+						</div>
+						{/* quote & message */}
+						{/* max-w-[90%] mx-auto */}{" "}
+						<div className='flex flex-1 flex-col justify-center before:w-[1px] xl:before:bg-white/20 xl:before:absolute xl:before:left-0 xl:before:h-[200px] relative xl:pl-20'>
+							{/* quote icon */}
+							<div className='mb-4'>
+								<FaQuoteLeft className='text-4xl xl:text-6xl text-white/20 mx-auto md:mx-0' />
+							</div>
+							{/* message */}
+							<p className='xl:text-lg text-center md:text-left'>
+								{person.message}
+							</p>
+						</div>
+					</div>
+				</SwiperSlide>
+			))}
+		</Swiper>
+	);
 };
 
 export default TestimonialSlider;
