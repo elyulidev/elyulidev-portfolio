a5180fab79eb80dbc71500f3600631b9c6fcd1f3
elyulidev <elyuli.dev@gmail.com>
2024-03-01 19:54:32 +0200

    WorkSlider: remove Image component

--- src/components/WorkSlider.jsx
+++ src/components/WorkSlider.jsx
@@ -5,27 +5,31 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/free-mode";
 import "swiper/css/pagination";
-import Image from "next/image";
+
 // data
 const workSlides = {
 	slides: [
 		{
 			images: [
 				{
-					title: "title",
+					title: "bioqApp",
 					path: "/thumb1.jpg",
+					img: "bg-bioqApp",
 				},
 				{
 					title: "title",
 					path: "/thumb2.jpg",
+					img: "bg-thumb2",
 				},
 				{
 					title: "title",
 					path: "/thumb3.jpg",
+					img: "bg-thumb3",
 				},
 				{
 					title: "title",
 					path: "/thumb4.jpg",
+					img: "bg-thumb4",
 				},
 			],
 		},
@@ -34,18 +38,22 @@ const workSlides = {
 				{
 					title: "title",
 					path: "/thumb4.jpg",
+					img: "bg-thumb4",
 				},
 				{
 					title: "title",
 					path: "/thumb1.jpg",
+					img: "bg-bioqApp",
 				},
 				{
 					title: "title",
 					path: "/thumb2.jpg",
+					img: "bg-thumb2",
 				},
 				{
 					title: "title",
 					path: "/thumb3.jpg",
+					img: "bg-thumb3",
 				},
 			],
 		},
@@ -69,9 +77,11 @@ const WorkSlider = () => {
 								key={index}
 								className='relative rounded-lg overflow-hidden flex items-center justify-center group'
 							>
-								<div className='flex items-center justify-center relative overflow-hidden group'>
+								<div
+									className={`${image.img} bg-cover bg-no-repeat flex items-center justify-center relative overflow-hidden group w-full h-[200px]`}
+								>
 									{" "}
-									<Image src={image.path} alt='' width={500} height={200} />
+									{/* <Image src={image.path} alt='' width={500} height={200} /> */}
 									{/* overlay gradient */}
 									<div className='absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700'></div>
 									{/* title */}
