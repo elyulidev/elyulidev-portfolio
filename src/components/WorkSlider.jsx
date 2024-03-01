e747f78fb3ce9556e9dd6aba0258cb7c69e36181
elyulidev <elyuli.dev@gmail.com>
2024-03-01 20:25:27 +0200

    WorkSlider: paste a real link BioqApp

--- src/components/WorkSlider.jsx
+++ src/components/WorkSlider.jsx
@@ -13,7 +13,7 @@ const workSlides = {
 			images: [
 				{
 					title: "bioqApp",
-					path: "/thumb1.jpg",
+					path: "https://bioquimica-app.vercel.app/",
 					img: "bg-bioqApp",
 				},
 				{
@@ -73,34 +73,38 @@ const WorkSlider = () => {
 				<SwiperSlide key={index}>
 					<div className='grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer'>
 						{slide.images.map((image, index) => (
-							<div
+							<a
 								key={index}
-								className='relative rounded-lg overflow-hidden flex items-center justify-center group'
+								href={image.path}
+								target='_blank'
+								rel='noopener noreferrer'
 							>
-								<div
-									className={`${image.img} bg-cover bg-no-repeat flex items-center justify-center relative overflow-hidden group w-full h-[200px]`}
-								>
-									{" "}
-									{/* <Image src={image.path} alt='' width={500} height={200} /> */}
-									{/* overlay gradient */}
-									<div className='absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700'></div>
-									{/* title */}
-									<div className='absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300'>
-										<div className='flex items-center gap-x-2 text-[13px] tracking-[0.2em]'>
-											{/* title part 1*/}
-											<div className='delay-100'>LIVE</div>
-											{/* title part 2*/}
-											<div className='translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150'>
-												PROJECT
-											</div>
-											{/* title part 3*/}
-											<div className=''>
-												<BsArrowRight />
+								<div className='relative rounded-lg overflow-hidden flex items-center justify-center group'>
+									<div
+										className={`${image.img} bg-cover bg-no-repeat flex items-center justify-center relative overflow-hidden group w-full h-[200px]`}
+									>
+										{" "}
+										{/* <Image src={image.path} alt='' width={500} height={200} /> */}
+										{/* overlay gradient */}
+										<div className='absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700'></div>
+										{/* title */}
+										<div className='absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300'>
+											<div className='flex items-center gap-x-2 text-[13px] tracking-[0.2em]'>
+												{/* title part 1*/}
+												<div className='delay-100'>LIVE</div>
+												{/* title part 2*/}
+												<div className='translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150'>
+													PROJECT
+												</div>
+												{/* title part 3*/}
+												<div className=''>
+													<BsArrowRight />
+												</div>
 											</div>
 										</div>
 									</div>
 								</div>
-							</div>
+							</a>
 						))}
 					</div>
 				</SwiperSlide>
